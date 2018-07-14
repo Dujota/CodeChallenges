9d40a2f5ece884adce7ed87cdb0ccce7ce467ae1
Denis <ddujota@internationalsafety.com>
2018-07-14 16:08:25 -0400

    all instructions are completed and ready for challenge

--- howManySeconds/getSeconds.js
+++ howManySeconds/getSeconds.js
@@ -1,6 +1,13 @@
 /*
 Given the list in reports.html of reports ran, console log the total run time in seconds of all Calculate Pricing reports
-Hints:
-1.) Select all the list items on the page and convert to array
-
 */
+
+// Select all the list items on the page and convert to array
+
+// Filter for only the elements that contain the word 'pricing'
+
+// map down to a list of time strings
+
+// map to an array of seconds
+
+// reduce to get total
