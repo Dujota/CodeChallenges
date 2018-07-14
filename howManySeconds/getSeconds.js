96bfee4e56049b2c26ef9bcb426a50eb0e10be51
Denis <ddujota@internationalsafety.com>
2018-07-14 15:43:01 -0400

    question almost complete

---
+++ howManySeconds/getSeconds.js
@@ -0,0 +1,6 @@
+/*
+Given the list in reports.html of reports ran, console log the total run time in seconds of all Calculate Pricing reports
+Hints:
+1.) Select all the list items on the page and convert to array
+
+*/
